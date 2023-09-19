b60fd5d89f189f674d791f882461232597f7ba3b
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-09-19 20:25:04 +0200

    Added DepartmentController with init method

---
+++ src/main/java/se/lu/ics/controllers/DepartmentController.java
@@ -0,0 +1,39 @@
+package se.lu.ics.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import se.lu.ics.models.Department;
+import se.lu.ics.data.DepartmentDAO;
+
+public class DepartmentController {
+    @FXML
+    private TableView<Department> tableViewDepartment;
+    @FXML
+    private TableColumn<Department, String> tableColumnDepartmentName;
+    @FXML
+    private TableColumn<Department, String> tableColumnDepartmentAddress;
+    @FXML
+    private TableColumn<Department, Integer> tableColumnDepartmentBudget;
+    @FXML
+    private TextField textFieldDepartmentName;
+    @FXML
+    private TextField textFieldDepartmentAddress;
+    @FXML
+    private TextField textFieldDepartmentBudget;
+    @FXML
+    private Button buttonDepartmentAdd;
+
+    public void initialize() {
+        tableColumnDepartmentName.setCellValueFactory(new PropertyValueFactory<Department, String>("departmentName"));
+        tableColumnDepartmentAddress.setCellValueFactory(new PropertyValueFactory<Department, String>("departmentAddress"));
+        tableColumnDepartmentBudget.setCellValueFactory(new PropertyValueFactory<Department, Integer>("departmentBudget"));
+
+        tableViewDepartment.setItems(DepartmentDAO.getDepartments());
+    }
+    
+}
