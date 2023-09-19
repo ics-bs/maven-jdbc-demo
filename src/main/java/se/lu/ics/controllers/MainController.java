9e82050da377d6f12125dc6f53dfe0ffa7dbf336
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-09-19 22:54:23 +0200

    Added MainController

---
+++ src/main/java/se/lu/ics/controllers/MainController.java
@@ -0,0 +1,16 @@
+package se.lu.ics.controllers;
+
+public class MainController {
+
+    /*
+     * MainController has no behavior at the moment as
+     * DepartmentController and EmployeeController handle
+     * All events of the nested fxml files
+     * 
+     * If we were to add additional elements to  Main.fxml
+     * apart from the nexted fxml files, we would need to
+     * add the appropriate event handlers here
+     * 
+     */
+
+}
